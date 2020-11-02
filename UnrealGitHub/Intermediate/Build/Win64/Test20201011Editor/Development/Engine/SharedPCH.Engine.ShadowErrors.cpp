729690020e05d4f63e397f70ce118fbf89abe8a0
ProJamesEun <44720532+projameseun@users.noreply.github.com>
2020-11-03 03:27:07 +0900

    test
    test

--- UnrealGitHub/Intermediate/Build/Win64/Test20201011Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/user/Desktop/Unreal/Test/Test20201011/Intermediate/Build/Win64/Test20201011Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- UnrealGitHub/Source/Test20201011/Test20201011.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Test20201011.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test20201011, "Test20201011" );
